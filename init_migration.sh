4e4e075e379ae3825306ce32a91cce425b968162
eklicious <eugenekang@gmail.com>
2019-04-19 13:50:51 -0400

    Update init_migration.sh

--- init_migration.sh
+++ init_migration.sh
@@ -7,9 +7,9 @@
 # The user will be prompted to start the app servers again if they like
 # This script requires the following parameters:
 # src, e.g. objectrocket uri
-# dest, e.g. mongodb+srv://eugenekang:password@cmx-qa-9h29h.mongodb.net/test?retryWrites=true
-# db, e.g. changestreamtest
-# coll, e.g. randomcollection
+# dest, e.g. atlas srv
+# db, e.g. db name
+# coll, e.g. collection name
 
 if [ "$#" -ne 4 ]; then
     echo "Illegal number of parameters. Following parameters are required: source uri, destination uri, database name, collection name"
